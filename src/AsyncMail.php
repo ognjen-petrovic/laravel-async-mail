0282ce726ea072b5d05c8fb022fcad6648f843c6
Ognjen <ognjen@heliozgroup.com>
2022-02-10 13:15:46 +0100

    use start

--- src/AsyncMail.php
+++ src/AsyncMail.php
@@ -9,7 +9,7 @@ class AsyncMail
     public static function send($mailable)
     {
         $process = Process::fromShellCommandline('php "${:ARTISAN}" "${:NAME}" "${:MAIL}"');
-        $process->run(null, ['ARTISAN' => base_path('artisan'), 
+        $process->start(null, ['ARTISAN' => base_path('artisan'), 
                              'NAME'=> 'async:mail',
                              'MAIL' => base64_encode(serialize($mailable))
                             ]);
