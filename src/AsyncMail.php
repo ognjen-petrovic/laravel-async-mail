d173ed90bcddc9071d2943337bf335938804b635
Ognjen <ognjen@heliozgroup.com>
2022-02-10 12:54:21 +0100

    Update process

--- src/AsyncMail.php
+++ src/AsyncMail.php
@@ -8,20 +8,10 @@ class AsyncMail
 {
     public static function send($mailable)
     {
-        $param = base64_encode(serialize($mailable));
-        $command = self::createCommandString($param);
-        $process = new Process([$command]);
-        $process->run();
-    }
-
-    protected static function createCommandString($param)
-    {
-        if (defined('PHP_WINDOWS_VERSION_BUILD'))
-        {
-            return 'start /B php ' . base_path('artisan') . ' async:mail ' . $param . ' > NUL';
-        } else
-        {
-            return 'php ' . base_path('artisan') . ' async:mail ' . $param . ' > /dev/null 2>&1 &';
-        }
+        $process = Process::fromShellCommandline('php "${:ARTISAN}" "${:NAME}" "${:MAIL}"');
+        $process->run(null, ['ARTISAN' => base_path('artisan'), 
+                             'NAME'=> 'async:mail',
+                             'MAIL' => base64_encode(serialize($mailable))
+                            ]);
     }
 }
